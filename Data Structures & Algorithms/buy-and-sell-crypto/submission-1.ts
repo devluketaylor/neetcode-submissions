5f7bb2f2ea33a4357d83faf646790d539cc816a8
Luke Taylor <117399678+devluketaylor@users.noreply.github.com>
2026-08-26 23:04:34 -0500

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.ts
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let minPrice = prices[0];
+        let maxProfit = 0;
+
+        for (const price of prices) {
+            minPrice = Math.min(minPrice, price);
+            maxProfit = Math.max(maxProfit, price - minPrice)
+        }
+
+        return maxProfit;
+    }
+}
